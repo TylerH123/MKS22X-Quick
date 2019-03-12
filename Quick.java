7d76b246013dbebfca58e5c0a348a72c71a250fa
Tyler Huang <thuang00@stuy.edu>
2019-03-12 14:50:45 -0400

    debugging

--- Quick.java
+++ Quick.java
@@ -9,60 +9,86 @@ public class Quick{
   }
   public static String toString(int[] arr){
     String output = "[";
-    for (int i = 0; i < arr.length; i++){
+    for (int i = 0; i < arr.length-1; i++){
       output += arr[i] + ", ";
     }
-    output += "]";
+    output += arr[arr.length-1] + "]";
     return output;
   }
+  public static int generateMedian(int length){
+    Random rand = new Random();
+    int a = rand.nextInt(length);
+    int b = rand.nextInt(length);
+    int c = rand.nextInt(length);
+    System.out.println(a + " " + b + " " + c);
+    if (a <= b && b <= c ){
+      return b;
+    }
+    if (b <= a  && a <= c ){
+      return a;
+    }
+    else if (b <= c && c <= a){
+      return c;
+    }
+    return -1;
+  }
   public static int quickselect(int[] data, int k){
-    int start = 0;
-    int end = data.length - 1;
-    while(true){
-      int pivot = partition(data,start,end,end-start + 1);
-      //if pivot equals k then return the num at k
-      if (k == pivot){
-        return data[k];
-      }
+    int lo = 1;
+    int hi = data.length - 1;
+    int pivot = partition(data,lo,hi);
+    System.out.println("toString" + toString(data));
+    System.out.println("pivot: " + pivot);
+    while(k != pivot) {
       //if k is less than pivot look from beginning to the pivot
       if (k < pivot){
-        end = pivot;
+        hi = pivot;
       }
-      //else look at the end startingg from pivot
-      else{
-        start = pivot;
+      //else look at the hi loingg from pivot
+      else if (k > pivot){
+        lo = pivot;
       }
+      pivot = partition(data,lo,hi);
+      System.out.println(pivot);
+      System.out.println("toString" + toString(data));
     }
+    return data[k];
   }
   //chooses a pivot and reorders the array
-  public static int partition(int[] data, int start, int end, int length){
+  public static int partition(int[] data, int lo, int hi){
     Random rand = new Random();
     //index of pivot value
-    int pivot = rand.nextInt(length);
+    int pivot = rand.nextInt(hi-lo+2);
+    System.out.println("pivot #: " + pivot + " value: " + data[pivot]);
     //swap pivot to the first number of index
     swap(data,0,pivot);
-    while (start != end){
-      //if val is bigger than pivot val then swap to end and end moves back
-      if (data[start] > data[pivot]){
-        swap(data,start, end);
-        end--;
+    System.out.println("partition first toString: " + toString(data));
+    while (lo != hi){
+      //if val is bigger than pivot val then swap to hi and hi moves back
+      if (data[lo] > data[0]){
+        swap(data,lo, hi);
+        hi--;
+        System.out.println("partition toString: " + toString(data));
       }
-      //if val is smaller than pivot val then do not swap, but instead start moves forward
-      if (data[start] < data[pivot]){
-        start++;
+      //if val is smaller than pivot val then do not swap, but instead lo moves forward
+      if (data[lo] < data[0]){
+        lo++;
+        System.out.println("partition toString: " + toString(data));
       }
     }
     //if the val is bigger than pivot val then swap with the value before it
-    if (data[start] < data[pivot]){
-      start--;
+    if (data[lo] < data[0]){
+      lo--;
     }
-    swap(data,start,pivot);
-    //return the index of start val
-    return start;
+    swap(data,0,lo);
+    //return the index of lo val
+    return lo;
   }
   public static void main(String[] args){
     int[] data = new int[]{9,19,8,1,12,99,10};
-    System.out.println(quickselect(data,1));
-    System.out.print(toString(data));
+    System.out.println(partition(data,1,6));
+    //System.out.println("sorted: [1,8,9,10,12,19,99]");
+    //System.out.println(quickselect(data,1));
+    //System.out.println(toString(data));
+    //System.out.println(generateMedian(7));
   }
 }
